ba065dd58d318e47653a28969b37a53510bea535
Da Silva Felix Bruno <brunolok79@gmail.com>
2025-04-28 00:00:04 -0300

    se amplio la ruta de /recommendations para buscar usuarios en la base de datos validarlos y obtener sus preferencias para buscar libros de posible interes

--- src/router/bookRecommendationRouter.py
+++ src/router/bookRecommendationRouter.py
@@ -1,7 +1,28 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from src.utils.databaseUtilities import get_document_by_id
+from src.utils.requestProcessingUtilities import responseGenerator,retrieveDocsForCategory
+from src.components.classEmbeddings import vector_store
+
 
 bookRecommendationRouter =APIRouter()
 
+class DataUser(BaseModel):
+     userId: str
+
 @bookRecommendationRouter.get('/recommendations',tags=['bookRecommendation'])
-def bookRecommendation():
-     return 'hello world'
+def bookRecommendation(req:DataUser):
+     user = get_document_by_id('userService','users',req.userId)
+
+     if not user:
+          return JSONResponse({'msg':'usuario no registrado'},status_code=404)
+
+     preference = user['preference']
+
+     searchedBooks = retrieveDocsForCategory(vector_store,preference)
+
+     print(searchedBooks)
+
+     return 'hola'
+
