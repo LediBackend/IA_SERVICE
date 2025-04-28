1adbad33f4dc28b9d0ce6948d89162ef22647e28
Da Silva Felix Bruno <brunolok79@gmail.com>
2025-04-28 02:43:07 -0300

    se le añadio el bloque try except para prevenir paros en el codigo

--- src/router/bookReceiveRouter.py
+++ src/router/bookReceiveRouter.py
@@ -10,46 +10,40 @@ bookReceiveRouter = APIRouter()
     
 
 @bookReceiveRouter.post('/upload',tags=['Books'])
-async def bookReceive(category:str = Body(...),
-    language:str = Body(...),file:UploadFile = File(...)):
+async def bookReceive(
+    category: str = Body(...),
+    language: str = Body(...),
+    file: UploadFile = File(...),
+):
+    UPLOAD_FOLDER = "./src/uploads"
+    
+    try:
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+        FileName = file.filename
 
-     UPLOAD_FOLDER = './src/uploads'
-     
-     os.makedirs(UPLOAD_FOLDER,exist_ok=True)
+        FilePath = await processedAndSaved(UPLOAD_FOLDER, file, FileName)
+        FileHash = getFileHash(FilePath)
 
-     FileName = file.filename
-     
-     FilePath = await processedAndSaved(UPLOAD_FOLDER,file,FileName)
-     
-     FileHash = getFileHash(FilePath)
+        if isPdfAlreadyIndexed(FileHash, vector_store):
+            return {"message": "Este libro ya ha sido indexado."}
 
-     isAlready = isPdfAlreadyIndexed(FileHash,vector_store)
+        document = loadFile(FilePath)
 
-     if isAlready:
-        
-        return {"message": "Este libro ya ha sido indexado."}
-     
-     document = loadFile(FilePath)
-     
-     isInstance = isinstance(document, str)
+        if isinstance(document, str):
+            document = [Document(page_content=document)]
 
-     if isInstance:
-         
-         document = [Document(page_content=document)]
+        ChunkedDocument = textSplitter(document)
 
-     ChunkedDocument = textSplitter(document)
+        for doc in ChunkedDocument:
+            doc.metadata["file_hash"] = FileHash
+            doc.metadata["book_name"] = FileName
+            doc.metadata["language"] = language
+            doc.metadata["category"] = translateToEnglish(category.lower())
 
-     for doc in ChunkedDocument:
-        
-        doc.metadata["file_hash"] = FileHash
-        
-        doc.metadata["book_name"] = FileName
+        indexDocs(ChunkedDocument, vector_store)
 
-        doc.metadata["language"] = language
+        return {"file_name": FileName, "status": "Libro subido y procesado correctamente"}
 
-        doc.metadata['category'] = translateToEnglish(category.lower())
+    except Exception as e:
+        return {"error": f"Ocurrió un error: {str(e)}"}
 
-
-     indexDocs(ChunkedDocument,vector_store)
-
-     return {"file_name": FileName, "status": "Libro subido y procesado correctamente"}
